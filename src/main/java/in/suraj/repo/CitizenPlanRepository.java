328bc613ea47f208a787b12b58531bb915c1e062
Surajjj <surajs07711@gmail.com>
2023-04-07 19:41:19 +0530

    first mini project

---
+++ src/main/java/in/suraj/repo/CitizenPlanRepository.java
@@ -0,0 +1,9 @@
+package in.suraj.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.suraj.entity.CitizenPlan;
+
+public interface CitizenPlanRepository extends JpaRepository<CitizenPlan, Integer>{
+
+}

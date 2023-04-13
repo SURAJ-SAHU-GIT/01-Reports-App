ad167b43adf80ece4c8246a1be16398e679e3364
Surajjj <surajs07711@gmail.com>
2023-04-13 09:49:53 +0530

    01 mini project

--- src/main/java/in/suraj/service/ReportServiceImpl.java
+++ src/main/java/in/suraj/service/ReportServiceImpl.java
@@ -29,12 +29,24 @@ import com.lowagie.text.pdf.PdfWriter;
 import in.suraj.entity.CitizenPlan;
 import in.suraj.repo.CitizenPlanRepository;
 import in.suraj.request.SearchRequest;
+import in.suraj.util.EmailUtils;
+import in.suraj.util.ExcelGenerator;
+import in.suraj.util.PdfGenerator;
 
 @Service
 public class ReportServiceImpl implements ReportService {
 
 	@Autowired
 	private CitizenPlanRepository planRepo;
+	
+	@Autowired
+	private ExcelGenerator excelGenerator;
+	
+	@Autowired
+	private PdfGenerator pdfGenerator;
+	
+	@Autowired
+	private EmailUtils emailUtils;
 
 	@Override
 	public List<String> getPlanNames() {
@@ -84,116 +96,38 @@ public class ReportServiceImpl implements ReportService {
 	@Override
 	public boolean exportExcel(HttpServletResponse response) throws Exception {
 
-		// Workbook workbook= new XSSFWorkbook(); //then extension will be xlsx
-		Workbook workbook = new HSSFWorkbook();
-		Sheet sheet = workbook.createSheet("plans-data");
-		Row headerRow = sheet.createRow(0);
-
-		headerRow.createCell(0).setCellValue("ID");
-		headerRow.createCell(1).setCellValue("Citizen Name");
-		headerRow.createCell(2).setCellValue("Plan Name");
-		headerRow.createCell(3).setCellValue("Plan Status");
-		headerRow.createCell(4).setCellValue("Plan Start Date");
-		headerRow.createCell(5).setCellValue("Plan End Date");
-		headerRow.createCell(6).setCellValue("Plan Benefit Amt");
-
-		List<CitizenPlan> records = planRepo.findAll();
-
-		int dataRowIndex = 1;
-
-		for (CitizenPlan plan : records) {
-			Row dataRow = sheet.createRow(dataRowIndex);
-			dataRow.createCell(0).setCellValue(plan.getCitizenId());
-			dataRow.createCell(1).setCellValue(plan.getCitizenName());
-			dataRow.createCell(2).setCellValue(plan.getPlanName());
-			dataRow.createCell(3).setCellValue(plan.getPlanStatus());
-
-			if (null != plan.getPlanStartDate()) {
-				dataRow.createCell(4).setCellValue(plan.getPlanStartDate() + "");
-			} else {
-				dataRow.createCell(4).setCellValue("N/A");
-			}
-
-			if (null != plan.getPlanEndDate()) {
-				dataRow.createCell(5).setCellValue(plan.getPlanEndDate() + "");
-			} else {
-				dataRow.createCell(5).setCellValue("N/A");
-			}
-
-			if (null != plan.getBenefitAmt()) {
-				dataRow.createCell(6).setCellValue(plan.getBenefitAmt());
-			} else {
-				dataRow.createCell(6).setCellValue("N/A");
-			}
-
-			dataRowIndex++;
-		}
-
-		ServletOutputStream outputStream = response.getOutputStream();
-		workbook.write(outputStream);
-		workbook.close();
+		File f = new File("Plans.xls");
+		
+		List<CitizenPlan> plans=planRepo.findAll();
+		excelGenerator.generate(response, plans, f);
+		
+		String subject= "Test mail subject";
+		String body= "<h1>Test mail body</h1>";
+		String to= "suraj120797@gmail.com";
+		
+		//File f = new File("Plans.xls");
+		
+		emailUtils.sendEmail(subject, body, to, f);
+		
+		f.delete();
 		return true;
 	}
 
 	@Override
 	public boolean exportPdf(HttpServletResponse response) throws Exception {
 
-		Document document = new Document(PageSize.A4);
-
-		PdfWriter.getInstance(document, response.getOutputStream());
-
-		document.open();
-
-		// Creating font
-		// Setting font style and size
-		Font fontTiltle = FontFactory.getFont(FontFactory.TIMES_ROMAN);
-		fontTiltle.setSize(20);
-
-		// Creating paragraph
-		Paragraph paragraph = new Paragraph("Citizen Plans", fontTiltle);
-
-		// Aligning the paragraph in document
-		paragraph.setAlignment(Paragraph.ALIGN_CENTER);
-
-		// Adding the created paragraph in document
-		document.add(paragraph);
-
-		PdfPTable table = new PdfPTable(6);
-
-		table.addCell("Id");
-		table.addCell("Citizen Name");
-		table.addCell("Plan Name");
-		table.addCell("Plan Status");
-		table.addCell("Start Date");
-		table.addCell("End Date");
-
-		List<CitizenPlan> plans = planRepo.findAll();
-
-		for (CitizenPlan plan : plans) {
-			table.addCell(String.valueOf(plan.getCitizenId()));
-			table.addCell(plan.getCitizenName());
-			table.addCell(plan.getPlanName());
-			table.addCell(plan.getPlanStatus());
-
-			if (null != plan.getPlanStartDate()) {
-				table.addCell(plan.getPlanStartDate() + "");
-			} else {
-				table.addCell("N/A");
-			}
-
-			if (null != plan.getPlanEndDate()) {
-				table.addCell(plan.getPlanEndDate() + "");
-			} else {
-				table.addCell("N/A");
-			}
-
-		}
+		File f = new File("Plans.pdf");
 		
-		document.add(table);
+		List<CitizenPlan> plans=planRepo.findAll();
+		pdfGenerator.generate(response, plans, f);
+		String subject= "Test mail subject";
+		String body= "<h1>Test mail body</h1>";
+		String to= "suraj120797@gmail.com";
+		
+		emailUtils.sendEmail(subject, body, to, f);
 
-		document.close();
-
-		return false;
+		f.delete();
+		return true;
 	}
 
 }
--- src/main/java/in/suraj/util/PdfGenerator.java
+++ src/main/java/in/suraj/util/PdfGenerator.java
@@ -1,5 +1,92 @@
 package in.suraj.util;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
+import in.suraj.entity.CitizenPlan;
+import in.suraj.repo.CitizenPlanRepository;
+
+@Component
 public class PdfGenerator {
+	
+	@Autowired
+	private CitizenPlanRepository planRepo;
+	
+	public void generate(HttpServletResponse response, List<CitizenPlan> plans, File f) throws Exception {
+		
+		Document document = new Document(PageSize.A4);
+
+		PdfWriter.getInstance(document, response.getOutputStream());
+		PdfWriter.getInstance(document, new FileOutputStream(f));
+		
+
+		document.open();
+
+		// Creating font
+		// Setting font style and size
+		Font fontTiltle = FontFactory.getFont(FontFactory.TIMES_ROMAN);
+		fontTiltle.setSize(20);
+
+		// Creating paragraph
+		Paragraph paragraph = new Paragraph("Citizen Plans", fontTiltle);
+
+		// Aligning the paragraph in document
+		paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+
+		// Adding the created paragraph in document
+		document.add(paragraph);
+
+		PdfPTable table = new PdfPTable(6);
+
+		table.addCell("Id");
+		table.addCell("Citizen Name");
+		table.addCell("Plan Name");
+		table.addCell("Plan Status");
+		table.addCell("Start Date");
+		table.addCell("End Date");
+
+		
+
+		for (CitizenPlan plan : plans) {
+			table.addCell(String.valueOf(plan.getCitizenId()));
+			table.addCell(plan.getCitizenName());
+			table.addCell(plan.getPlanName());
+			table.addCell(plan.getPlanStatus());
+
+			if (null != plan.getPlanStartDate()) {
+				table.addCell(plan.getPlanStartDate() + "");
+			} else {
+				table.addCell("N/A");
+			}
+
+			if (null != plan.getPlanEndDate()) {
+				table.addCell(plan.getPlanEndDate() + "");
+			} else {
+				table.addCell("N/A");
+			}
+
+		}
+		
+		document.add(table);
+
+		document.close();
+		
+	}
 
 }
